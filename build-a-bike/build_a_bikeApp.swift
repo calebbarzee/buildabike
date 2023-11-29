863190b7b855f488b5ea730d11fdd73483a612e1
calebbarzee <barzeec@gmail.com>
2023-11-29 12:02:48 -0700

    Initial Commit

---
+++ build-a-bike/build_a_bikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  build_a_bikeApp.swift
+//  build-a-bike
+//
+//  Created by Caleb Barzee on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct build_a_bikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
